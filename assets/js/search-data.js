ceb903ac74ba62731157d7864d1ebf4c8b016aae
XiaohanCai00 <xiaohan.cai2000@gmail.com>
2026-05-25 09:35:44 +0000

    Deploying to gh-pages from @ XiaohanCai00/xiaohancai00.github.io@b350956dd30ecae6cbca5cf517e3eb555392955c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Below is a brief version of my CV. The full CV is available via the button on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
